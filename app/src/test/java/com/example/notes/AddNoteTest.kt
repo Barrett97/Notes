23edc59798a19a18dd63bec7ecba4b96a2a34d38
Barret <tranbarrett@gmail.com>
2020-11-17 00:47:19 -0500

    Create AddNoteTest.kt

---
+++ app/src/test/java/com/example/notes/AddNoteTest.kt
@@ -0,0 +1,26 @@
+package com.example.notes
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.filters.LargeTest
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AddNoteTest {
+
+//    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>();
+//
+//    @Test
+//    fun addNote() {
+//        onView(withId(R.id.fab).perform)
+//    }
+
+}
