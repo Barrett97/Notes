ec212b236ddd68e6ff904a7da6adc0d3abfeee5f
Barret <tranbarrett@gmail.com>
2021-02-05 23:17:50 -0500

    fixup! Refactoring

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -16,7 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
-
+    //test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
